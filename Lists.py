5a9a919f069f9adeaae616be50cc87e0e52c75aa
adambensh <adam.bensh@gmail.com>
2018-07-25 22:52:18 +0300

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,26 @@
+import random
+
+# List Less than 10
+
+num_list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+for x in num_list:
+    if x < 5:
+        print x
+
+
+new_list = []
+
+for x in num_list:
+    if x < 5:
+        new_list.append(x)
+print(new_list)
+
+
+another_list = []
+
+user_input = input("Please type in a number to be the greater: ")
+for x in num_list:
+    if x < user_input:
+        another_list.append(x)
+print(another_list)
