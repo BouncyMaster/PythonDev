95e0eda5882e9e252bb308a8a81c6b0a5821ca95
adambensh <adam.bensh@gmail.com>
2018-07-25 23:16:33 +0300

    Create PrimeNumbers.py

---
+++ PrimeNumbers.py
@@ -0,0 +1,25 @@
+import random
+
+# Divisors
+
+num_list = []
+
+
+def load_list():
+    for x in range(2, 100):
+        num_list.append(x)
+
+
+load_list()
+result = ""
+num2check = input("Please enter a number to check for divisors: ")
+for x in num_list:
+    if x >= num2check:
+        break
+    elif num2check % x == 0:
+        result = "Number is diviseable!"
+        break
+    else:
+        result = "%d is a prime number!" % num2check
+
+print(result)
