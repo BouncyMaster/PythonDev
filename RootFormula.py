dc05165ba27e8fed143db88b4be3dbbd4cdc0928
BouncyMaster <37939917+BouncyMaster@users.noreply.github.com>
2018-08-03 11:46:53 +0300

    Update RootFormula.py

--- RootFormula.py
+++ RootFormula.py
@@ -1,19 +1,19 @@
 import math
 
-print "Please type in your coefficients: "
-firstCoeff = input("First coefficient: ")
-secondCoeff = input("Second coefficient: ")
-thirdCoeff = input("Third coefficient: ")
-print "a: %d b: %d c: %d" % (firstCoeff, secondCoeff, thirdCoeff)
+print("Please type in your coefficients: ")
+firstCoeff = int(input("First coefficient: "))
+secondCoeff = int(input("Second coefficient: "))
+thirdCoeff = int(input("Third coefficient: "))
+print("a: %d b: %d c: %d" % (firstCoeff, secondCoeff, thirdCoeff))
 
 root = math.sqrt((secondCoeff**2)-(4*firstCoeff*thirdCoeff))
 x1 = (-secondCoeff+root)/(2*firstCoeff)
 x2 = (-secondCoeff-root)/(2*firstCoeff)
 
 if x1 != x2:
-    print "The answers are: (x1, x2) (%.2f , %.2f)" % (x1, x2)
+    print("The answers are: (x1, x2) (%.2f , %.2f)" % (x1, x2))
 elif x1 == x2:
-    print "The answer is: x = %.2f" % x1
+    print("The answer is: x = %.2f" % x1)
 else:
     print("Something went wrong. Please try again.\n")
-mainLoop = raw_input("Press the Enter key to close this window.")
+mainLoop = input("Press the Enter key to close this window.")
